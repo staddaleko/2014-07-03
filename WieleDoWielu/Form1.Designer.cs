f2f93b5d61375f799ed2115fb52b5564ab9ef2ab
staddaleko <staddaleko@gmail.com>
2014-07-03 16:09:55 +0200

    nowy ficzer - zamykanie bez błędów!

--- WieleDoWielu/Form1.Designer.cs
+++ WieleDoWielu/Form1.Designer.cs
@@ -40,6 +40,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ksiazkiDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.autorzyBindingSource)).BeginInit();
             this.SuspendLayout();
@@ -52,6 +53,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(385, 21);
             this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             this.comboBox1.Format += new System.Windows.Forms.ListControlConvertEventHandler(this.comboBox1_Format);
             // 
             // listBox1
@@ -86,24 +88,27 @@
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.autorzyBindingSource, "Nazwisko", true));
-            this.textBox1.Location = new System.Drawing.Point(481, 103);
+            this.textBox1.Location = new System.Drawing.Point(489, 103);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(140, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.autorzyBindingSource, "Imie", true));
-            this.textBox2.Location = new System.Drawing.Point(481, 163);
+            this.textBox2.Location = new System.Drawing.Point(489, 163);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(140, 20);
             this.textBox2.TabIndex = 2;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(478, 87);
+            this.label1.Location = new System.Drawing.Point(486, 87);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 13);
             this.label1.TabIndex = 3;
@@ -111,24 +116,39 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(478, 147);
+            this.label2.Location = new System.Drawing.Point(486, 147);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nazwisko";
             // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(500, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Zamknij!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 459);
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(653, 496);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.Color.Crimson;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -152,6 +172,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WieleDoWielu/Form1.cs
+++ WieleDoWielu/Form1.cs
@@ -21,6 +21,8 @@ namespace WieleDoWielu
         {
             // TODO: This line of code loads data into the 'ksiazkiDataSet.Autorzy' table. You can move, or remove it, as needed.
             this.autorzyTableAdapter.Fill(this.ksiazkiDataSet.Autorzy);
+            ksiazkiTableAdapter1.Fill(ksiazkiDataSet.Ksiazki);
+            ksiazkaAutorTableAdapter1.Fill(ksiazkiDataSet.KsiazkaAutor);
 
         }
 
@@ -29,6 +31,26 @@ namespace WieleDoWielu
             DataRowView rowView = e.ListItem as DataRowView;
             KsiazkiDataSet.AutorzyRow row = rowView.Row as KsiazkiDataSet.AutorzyRow;
             e.Value = row.Nazwisko + " " + row.Imie;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex==-1)
+            {
+                return;
+            }
+            listBox1.Items.Clear();
+            DataRowView rowViewAutorzy = comboBox1.SelectedItem as DataRowView;
+            KsiazkiDataSet.AutorzyRow rowAutorzy = rowViewAutorzy.Row as KsiazkiDataSet.AutorzyRow;
+            foreach (var row in rowAutorzy.GetKsiazkaAutorRows())
+            {
+                listBox1.Items.Add(row.KsiazkiRow.Tytul);
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
